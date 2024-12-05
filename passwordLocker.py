fffbe82672607b4427a786e20e7b789ac4526de6
GoodGuyGregory <greg.witt625@gmail.com>
2024-12-05 10:27:22 -0800

    adds ":"

--- passwordLocker.py
+++ passwordLocker.py
@@ -17,9 +17,4 @@ if account in PASSWORDS:
     pyperclip.copy(PASSWORDS[account])
     print('Password for ' + account + ' copied to clipboard.')
 else:
-<<<<<<< HEAD
-    print('There is no account named ' + account)
-print('Password for ' + account + ' copied to clipboard.')
-=======
     print('There is no account named: ' + account)
->>>>>>> d2ed483 (adds ":" for can't find)
